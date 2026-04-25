fe2997ac904486691efe975b6a4df3de1dee1353
Andrii Fedorchenkov <well.well.well.pink@gmail.com>
2026-04-25 19:13:31 +0300

    Feature: add SatellitePhone and LandlinePhone to the hierarchy

---
+++ src/main/java/ua/edu/sumdu/LandlinePhone.java
@@ -0,0 +1,52 @@
+package ua.edu.sumdu;
+
+import java.util.Objects;
+
+public class LandlinePhone extends Phone {
+
+    private boolean isCordless;
+
+    public LandlinePhone(String brand, String model, double price, int storageCapacity, boolean isCordless) {
+        super(brand, model, price, storageCapacity);
+        this.isCordless = isCordless;
+    }
+
+    public boolean isCordless() {
+        return isCordless;
+    }
+
+    public void setCordless(boolean isCordless) {
+        this.isCordless = isCordless;
+    }
+
+    @Override
+    public String toString() {
+        return "LandlinePhone{"
+                + "brand='" + getBrand() + '\''
+                + ", model='" + getModel() + '\''
+                + ", price=" + getPrice()
+                + ", storageCapacity=" + getStorageCapacity() + " GB"
+                + ", isCordless=" + isCordless
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        LandlinePhone that = (LandlinePhone) o;
+        return isCordless == that.isCordless;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), isCordless);
+    }
+}
---
+++ src/main/java/ua/edu/sumdu/SatellitePhone.java
@@ -0,0 +1,55 @@
+package ua.edu.sumdu;
+
+import java.util.Objects;
+
+public class SatellitePhone extends Phone {
+
+    private String satelliteNetwork;
+
+    public SatellitePhone(String brand, String model, double price, int storageCapacity, String satelliteNetwork) {
+        super(brand, model, price, storageCapacity);
+        setSatelliteNetwork(satelliteNetwork);
+    }
+
+    public String getSatelliteNetwork() {
+        return satelliteNetwork;
+    }
+
+    public void setSatelliteNetwork(String satelliteNetwork) {
+        if (satelliteNetwork == null || satelliteNetwork.isBlank()) {
+            throw new IllegalArgumentException("Satellite network must not be null or empty.");
+        }
+        this.satelliteNetwork = satelliteNetwork;
+    }
+
+    @Override
+    public String toString() {
+        return "SatellitePhone{"
+                + "brand='" + getBrand() + '\''
+                + ", model='" + getModel() + '\''
+                + ", price=" + getPrice()
+                + ", storageCapacity=" + getStorageCapacity() + " GB"
+                + ", satelliteNetwork='" + satelliteNetwork + '\''
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        SatellitePhone that = (SatellitePhone) o;
+        return Objects.equals(satelliteNetwork, that.satelliteNetwork);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), satelliteNetwork);
+    }
+}
